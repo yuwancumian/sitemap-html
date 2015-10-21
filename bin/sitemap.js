ed37e884988e98789272b1edc747f8608e95400f
yuwancumian <easier2005@163.com>
2015-10-21 20:20:14 +0800

    add style

--- bin/sitemap.js
+++ bin/sitemap.js
@@ -28,7 +28,9 @@ function addTag(item){
 }
 ob.forEach(addTag);
 function buildHtml(body){
-	return '<!DOCTYPE html><html lang="zh_CN"><head><meta charset="UTF-8"></head><body>'
+	return '<!DOCTYPE html><html lang="zh_CN"><head><meta charset="UTF-8">'
+		+'<style>div{float:left;clear:both;width:100%;height:64px;line-height:64px;border-bottom:1px solid #c3c3c3;font-size:36px}a{float:left;width:30%} p{height:34px;width:50%;margin:0;float:right}</style>'
+		+'</head><body>'
 	+ body + '</body></html>'
 }
 var data = buildHtml(str); 
